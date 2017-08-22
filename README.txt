e1feef1c645c4b486c72ae6750b667542fb9e66f
Park Ji Wong <sailfish009@gmail.com>
2017-08-22 08:04:08 +0200

    update files

---
+++ README.txt
@@ -0,0 +1,4 @@
+Vdpau Sample Applicaton for orangepipc.
+
+This source is available under the zlib license 
+
--- sample.c
+++ sample.c
@@ -17,29 +17,11 @@ GLXContext              glc;
 XWindowAttributes       gwa;
 XEvent                  xev;
 
-void DrawAQuad() 
-{
-  glClearColor(1.0, 1.0, 1.0, 1.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
- 
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(-1., 1., -1., 1., 1., 20.);
- 
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
-  gluLookAt(0., 0., 10., 0., 0., 0., 0., 1., 0.);
- 
-  glBegin(GL_QUADS);
-  glColor3f(1., 0., 0.); glVertex3f(-.75, -.75, 0.);
-  glColor3f(0., 1., 0.); glVertex3f( .75, -.75, 0.);
-  glColor3f(0., 0., 1.); glVertex3f( .75,  .75, 0.);
-  glColor3f(1., 1., 0.); glVertex3f(-.75,  .75, 0.);
-  glEnd();
-} 
+GC                      gc;
  
 int main(int argc, char *argv[]) 
 {
+  unsigned long black, white;
   dpy = XOpenDisplay(NULL);
   
   if(dpy == NULL) {
@@ -48,6 +30,8 @@ int main(int argc, char *argv[])
   }
          
   root = DefaultRootWindow(dpy);
+  black = BlackPixel(dpy, DefaultScreen(dpy));
+  white = WhitePixel(dpy, DefaultScreen(dpy));
  
   vi = glXChooseVisual(dpy, 0, att);
  
@@ -64,11 +48,18 @@ int main(int argc, char *argv[])
   swa.colormap = cmap;
   swa.event_mask = ExposureMask | KeyPressMask;
   swa.override_redirect = True;
+  swa.background_pixel = white;
   XGetWindowAttributes(dpy, root, &gwa);
   win = XCreateWindow(dpy, root, 0, 0, gwa.width, gwa.height, 0, vi->depth, InputOutput, vi->visual, CWBorderPixel|CWColormap|CWEventMask|CWOverrideRedirect, &swa);
 
+  ///////////////////////////////////////////////////////// 
+  gc = XCreateGC(dpy, win, 0, NULL);
+  XSetBackground(dpy, gc, white);
+  XSetBackground(dpy, gc, black);
+  XClearWindow(dpy, win);
+  ///////////////////////////////////////////////////////// 
+
   XMapWindow(dpy, win);
-  //XStoreName(dpy, win, "VERY SIMPLE APPLICATION");
   
   glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
   glXMakeCurrent(dpy, win, glc);
@@ -77,12 +68,8 @@ int main(int argc, char *argv[])
   while(1) 
   {
     XNextEvent(dpy, &xev);
-    
     if(xev.type == Expose) {
-      XGetWindowAttributes(dpy, win, &gwa);
-      glViewport(0, 0, gwa.width, gwa.height);
-      DrawAQuad(); 
-      glXSwapBuffers(dpy, win);
+
     }
     else if(xev.type == KeyPress) {
       glXMakeCurrent(dpy, None, NULL);
